f2c6ba486c9d56632edfbd9932a3b45699ce3d14
moustafamorsy <mostafamorsyyy@gmail.com>
2022-10-27 16:32:55 +0200

    L-P

---
+++ project/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+  ],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px',
+    },
+    extend: {
+      colors:{
+        brightRed: 'hsl(12 , 88% ,59%)',
+        brightRedLight: 'hsl(12 , 88% ,69%)',
+        brightRedSupLight: 'hsl(12 , 88% ,95%)',
+        DarkBlue: 'hsl(228 , 39% ,23%)',
+        DarkGrayishBlue: 'hsl(227 , 12% ,61%)',
+        VeryDarkBlue: 'hsl(233 , 12% ,13%)',
+        VeryPaleRed: 'hsl(13 , 100% ,96%)',
+        VeryLightGray: 'hsl(0 , 0% ,98%)',
+      }
+    },
+  },
+  plugins: [],
+}
